9ba47fa0309a66e02c36103b7d4b42b5c87f5a9d
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-04-11 13:04:57 -0500

    feat: Implement by-country page with search functionality and error handling

--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,10 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {rxResource} from '@angular/core/rxjs-interop';
 import {CountryListComponent} from '@app/country/components/country-list/country-list.component';
 import {SearchInputComponent} from '@app/country/components/country-search-input/country-search-input.component';
+import {CountryService} from '@app/country/services/country.service';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'by-country-page',
   imports: [SearchInputComponent, CountryListComponent],
   templateUrl: './by-country-page.component.html',
 })
-export class ByCountryPageComponent { }
+export class ByCountryPageComponent {
+
+  countryService = inject(CountryService);
+  query = signal<string>('');
+
+  countryResource = rxResource({
+    request: () => ({query: this.query()}),
+    loader: ({request}) => {
+      if (!request.query) return of([]);
+
+      return this.countryService.searchByCountry(request.query)
+    }
+  })
+}
