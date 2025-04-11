f6fffdb42e4fd29f7a3f36ea2bea3214477ad7de
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-04-11 18:21:53 -0500

    feat: Update country search components to use linkedSignal for query management and improve routing

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,9 +1,9 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, inject, linkedSignal} from '@angular/core';
 import {rxResource} from '@angular/core/rxjs-interop';
+import {ActivatedRoute, Router} from '@angular/router';
 import {CountryListComponent} from '@app/country/components/country-list/country-list.component';
 import {SearchInputComponent} from '@app/country/components/country-search-input/country-search-input.component';
 import {CountryService} from '@app/country/services/country.service';
-
 import {of} from 'rxjs';
 
 @Component({
@@ -12,14 +12,23 @@ import {of} from 'rxjs';
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
-
   countryService = inject(CountryService);
-  query = signal<string>('');
+  router = inject(Router);
+  activatedRoute = inject(ActivatedRoute);
+
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+  query = linkedSignal(() => this.queryParam);
 
   countryResource = rxResource({
     request: () => ({query: this.query()}),
     loader: ({request}) => {
       if (!request.query) return of([]);
+
+      this.router.navigate(['/country/by-capital'], {
+        queryParams: {
+          query: request.query
+        }
+      });
 
       return this.countryService.searchByCapital(request.query)
     }
